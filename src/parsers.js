ff5f4d0f1fe55e529f0a8b299a5cc03bbc820440
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-03-09 19:03:09 +0300

    fix after code review #1

--- src/parsers.js
+++ src/parsers.js
@@ -3,16 +3,16 @@ import path from 'path';
 import { readFileSync } from 'fs';
 
 const readFile = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), '__fixtures__', filepath);
+  const absolutePath = path.resolve(process.cwd(), filepath);
   return readFileSync(absolutePath, 'utf-8');
 };
 
-const parse = (file, extension) => {
-  if (extension === '.json') {
-    return JSON.parse(file);
+const parse = (string, format) => {
+  if (format === '.json') {
+    return JSON.parse(string);
   }
-  if (extension === '.yml' || extension === '.yaml') {
-    return yaml.load(file);
+  if (format === '.yml' || format === '.yaml') {
+    return yaml.load(string);
   }
   throw new Error('not supported format');
 };
