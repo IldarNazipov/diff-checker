770ffc02dca98df9a064dc9bbd8d23e80d1eac13
Ildar Nazipov <ildar.nazipov@gmail.com>
2023-03-14 13:39:10 +0700

    fix after code review # 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import path from 'path';
+import { readFileSync } from 'fs';
 import compareObjects from './compareObjects.js';
-import { parse, readFile } from './parsers.js';
+import parse from './parsers.js';
 import chooseFormatter from './formatters/index.js';
 
-export default (filepath1, filepath2, formatName = 'stylish') => {
+const readFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  return readFileSync(absolutePath, 'utf-8');
+};
+
+const defineFileFormat = (filepath) => {
+  const fileExtension = path.extname(filepath);
+  if (fileExtension === '.json') {
+    return 'JSON';
+  }
+  if (fileExtension === '.yml' || fileExtension === '.yaml') {
+    return 'YAML';
+  }
+  throw new Error('not supported file format');
+};
+
+export default (filepath1, filepath2, outputFormatName = 'stylish') => {
   const fileContent1 = readFile(filepath1);
   const fileContent2 = readFile(filepath2);
-  const fileExtension1 = path.extname(filepath1);
-  const fileExtension2 = path.extname(filepath2);
-  const obj1 = parse(fileContent1, fileExtension1);
-  const obj2 = parse(fileContent2, fileExtension2);
+  const fileFormat1 = defineFileFormat(filepath1);
+  const fileFormat2 = defineFileFormat(filepath2);
+  const obj1 = parse(fileContent1, fileFormat1);
+  const obj2 = parse(fileContent2, fileFormat2);
   const sortedObjectsArray = compareObjects(obj1, obj2);
-  return chooseFormatter(formatName, sortedObjectsArray);
+  return chooseFormatter(outputFormatName, sortedObjectsArray);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,11 @@
 import yaml from 'js-yaml';
-import path from 'path';
-import { readFileSync } from 'fs';
 
-const readFile = (filepath) => {
-  const absolutePath = path.resolve(process.cwd(), filepath);
-  return readFileSync(absolutePath, 'utf-8');
-};
-
-const parse = (string, format) => {
-  if (format === '.json') {
-    return JSON.parse(string);
+export default (data, format) => {
+  if (format === 'JSON') {
+    return JSON.parse(data);
   }
-  if (format === '.yml' || format === '.yaml') {
-    return yaml.load(string);
+  if (format === 'YAML') {
+    return yaml.load(data);
   }
   throw new Error('not supported format');
 };
-
-export { parse, readFile };
